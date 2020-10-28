e002e841a2b3e4fa4a746af7086f66c0a7cc516b
riest817 <sano817@hotmail.co.jp>
2020-10-28 23:30:14 +0900

    change repository

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main{
     public static void main(String args[]){
       System.out.println("hello Java!");
-      System.out.println("うどん！うどん！うどん！うどん！うどん！うどん！うどん！うどん！うどん！うどん！");
+      System.out.println("うどん！うどん！うどん！うどん！うどん！");
       System.out.println("OVERWRITE!!!");
       System.out.println("OVERWRITE!!!");
     }
